a78b55817d03123fa30afe8fb03023bd1798d057
rishavmishra24 <30778335+rishavmishra24@users.noreply.github.com>
2020-10-26 19:15:01 +0530

    Update parser.h

--- parser.h
+++ parser.h
@@ -142,17 +142,17 @@ token *jaggedInitialisationList(token *node, treeNode *root2);
 token *jaggedInitialisationList2(token *node, treeNode *root2);
 
 //rishav
-token * varList(token *,treeNode *);
-token * varList2(token *,treeNode *);
-token * dimensions(token *,treeNode *);
-token * dimensions2(token *,treeNode *);
-token * range(token *,treeNode *);
-token * singleStaticDimension(token *,treeNode *)
-token * index(token *,treeNode *)
-token * index2(token *,treeNode *)
-token * brackets(token *,treeNode *)
-token * assignment(token *,treeNode *)
-token * varName(token *,treeNode *)
+token *varList(token *,treeNode *);
+token *varList2(token *,treeNode *);
+token *dimensions(token *,treeNode *);
+token *dimensions2(token *,treeNode *);
+token *range(token *,treeNode *);
+token *singleStaticDimension(token *,treeNode *);
+token *index1(token *,treeNode *);
+token *index2(token *,treeNode *);
+token *brackets(token *,treeNode *);
+token *assignment(token *,treeNode *);
+token *varName(token *,treeNode *);
 
 
 //sumit
